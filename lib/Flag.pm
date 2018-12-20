56fb08ae7e356bc8fdad6042d28d687271f09cfb
HaoDaWang <1306036576@qq.com>
2018-12-20 18:25:06 +0800

    Export var and parse

--- lib/Flag.pm
+++ lib/Flag.pm
@@ -3,18 +3,28 @@ package Flag;
 use 5.018002;
 use strict;
 use warnings;
+use Logger;
 
 require Exporter;
 
 our @ISA = qw(Exporter);
 
 our @EXPORT = qw(
-
+  var
+  parse
 );
 
 our $VERSION = '0.01';
 
 my %CLI_params = ();
+
+sub must {
+  my ($tag, $value) = @_;
+
+  if(!defined($value)){
+    die err("$tag cannot be undef");
+  }
+}
 
 sub is_param {
   my ($value) = @_;
@@ -25,18 +35,31 @@ sub is_param {
 }
 
 sub var {
+  my @tags = qw(
+    name
+    default
+    help
+  );
+
+  foreach (0..2) {
+    must($tags[$_], $_[$_]);
+  }
+
   my ($name, $default, $help) = @_;
   my %params = ('name'=>$name, 'default'=>$default, 'help'=>$help);
-
-  print "$name, $default, $help\n";
 
   $CLI_params{$name} = {%params};
 }
 
 sub parse {
-  my (@args) = @_;
+  my (@args) = @ARGV;
   my %params = ();
   my ($key, $value, $str);
+
+  # No register var or no CLI params
+  if(!scalar(@args) || !scalar(keys %CLI_params)){
+    die err("There are no parameters.");
+  }
 
   for(my $i = 0;$i < scalar @args;$i += 2){
     $str = $args[$i];
@@ -46,13 +69,13 @@ sub parse {
     if(!defined($value) || is_param($value)){
       my $help_info = $CLI_params{$key}{"help"};
 
-      die "Usage -$key $help_info\n";
+      die err("Usage: -$key <args> --$help_info\n");
     }
 
     $params{$key} = $value;
   }
 
-  return %params;
+  %params;
 }
 
 1;
