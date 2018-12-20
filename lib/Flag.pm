f1c85354dc40e5ca7a04ba4b065f80230a93ebb4
HaoDaWang <1306036576@qq.com>
2018-12-20 15:16:30 +0800

    Add error msg

--- lib/Flag.pm
+++ lib/Flag.pm
@@ -8,77 +8,51 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Flag ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
 our @EXPORT = qw(
-	
+
 );
 
 our $VERSION = '0.01';
 
+my %CLI_params = ();
 
-# Preloaded methods go here.
+sub is_param {
+  my ($value) = @_;
+  my @value_arr = split "", $value;
+  my $prefix = shift @value_arr;
+
+  $prefix eq "-";
+}
+
+sub var {
+  my ($name, $default, $help) = @_;
+  my %params = ('name'=>$name, 'default'=>$default, 'help'=>$help);
+
+  print "$name, $default, $help\n";
+
+  $CLI_params{$name} = {%params};
+}
+
+sub parse {
+  my (@args) = @_;
+  my %params = ();
+  my ($key, $value, $str);
+
+  for(my $i = 0;$i < scalar @args;$i += 2){
+    $str = $args[$i];
+    $key = substr $str, 1, length $str;
+    $value = $args[$i + 1];
+
+    if(!defined($value) || is_param($value)){
+      my $help_info = $CLI_params{$key}{"help"};
+
+      die "Usage -$key $help_info\n";
+    }
+
+    $params{$key} = $value;
+  }
+
+  return %params;
+}
 
 1;
-__END__
-# Below is stub documentation for your module. You'd better edit it!
-
-=head1 NAME
-
-Flag - Perl extension for blah blah blah
-
-=head1 SYNOPSIS
-
-  use Flag;
-  blah blah blah
-
-=head1 DESCRIPTION
-
-Stub documentation for Flag, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
-
-=head2 EXPORT
-
-None by default.
-
-
-
-=head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
-
-=head1 AUTHOR
-
-赵展浩, E<lt>haodawang@sd.apple.comE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright (C) 2018 by 赵展浩
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.18.2 or,
-at your option, any later version of Perl 5 you may have available.
-
-
-=cut
