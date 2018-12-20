89e819d37de6711ad0d4686c7218402786f3f582
HaoDaWang <1306036576@qq.com>
2018-12-20 15:38:13 +0800

    Add warn log info error for print msg

---
+++ lib/logger.pm
@@ -0,0 +1,55 @@
+package Logger;
+
+use 5.018002;
+use strict;
+use warnings;
+use Term::ANSIColor;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+  warn
+  err
+  log
+  info
+);
+
+use constant WARN_COLOR => "yellow";
+use constant INFO_COLOR => "blue";
+use constant ERROR_COLOR => "red";
+use constant LOG_COLOR => "white";
+
+sub print_color_msg {
+  my ($msg, $color, $is_bold) = @_;
+  my $bold = $is_bold || "bold";
+
+  print color "$bold $color";
+  print "$msg\n";
+  print color "reset";
+}
+
+sub warn {
+  my ($msg) = @_;
+
+  print_color_msg($msg, WARN_COLOR);
+}
+
+sub error {
+  my ($msg) = @_;
+
+  print_color_msg($msg, ERROR_COLOR);
+}
+
+sub info {
+  my ($msg) = @_;
+
+  print_color_msg($msg, INFO_COLOR);
+}
+
+sub log {
+  my ($msg) = @_;
+  $msg = "";
+
+  print_color_msg($msg, LOG_COLOR);
+}
